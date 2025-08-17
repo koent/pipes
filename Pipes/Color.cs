889f215cd1f0ce0adc83015af3230578ac934feb
koent <ktbbb5@gmail.com>
2025-08-17 14:12:31 +0200

    Vector math

---
+++ Pipes/Color.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Pipes;
+
+public struct Color(float red, float green, float blue)
+{
+    public readonly float Red => red;
+    public readonly float Green => green;
+    public readonly float Blue => blue;
+
+    public static Color FromHue(float hue) => new
+    (
+        .5f + .5f * MathF.Cos(hue),
+        .5f + .5f * MathF.Cos(hue + MathF.PI * 2f / 3f),
+        .5f + .5f * MathF.Cos(hue + MathF.PI * 4f / 3f)
+    );
+}
--- Pipes/State.cs
+++ Pipes/State.cs
@@ -1,6 +1,4 @@
 using System;
-using Vec2 = (float X, float Y);
-using Color = (float R, float G, float B);
 using System.Linq;
 
 namespace Pipes;
@@ -20,16 +18,16 @@ public class State(int maxNofVerticesAndTriangles, float pipeWidth, float speed)
     public int VertexArrayLength => FloatsPerVertex * (int)NofVertices;
     public int IndexArrayLength => UIntsPerTriangle * (int)NofTriangles;
 
-    public Vec2 Scale { get; private set; } = (1.0f, 1.0f);
+    public float Scale { get; private set; } = 1.0f; // In X direction
 
     private bool Started => NofVertices > 0;
-    public Vec2 Position { get; set; }
-    private Vec2 Velocity { get; set; }
+    public Vector2 Position { get; set; }
+    private Vector2 Velocity { get; set; }
     private Color Color { get; set; }
     private float Hue { get; set; }
     public Direction Direction { get; private set; }
 
-    public bool OutOfBounds => MathF.Abs(Position.X * Scale.X) > 1.0f || MathF.Abs(Position.Y * Scale.Y) > 1.0f;
+    public bool OutOfBounds => MathF.Abs(Position.X * Scale) > 1.0f || MathF.Abs(Position.Y) > 1.0f;
 
     public void Clear(float? newScale = null)
     {
@@ -38,7 +36,7 @@ public class State(int maxNofVerticesAndTriangles, float pipeWidth, float speed)
 
         if (newScale.HasValue)
         {
-            Scale = (newScale.Value, 1.0f);
+            Scale = newScale.Value;
         }
     }
 
@@ -46,7 +44,7 @@ public class State(int maxNofVerticesAndTriangles, float pipeWidth, float speed)
         => NofVertices + 4 <= maxNofVerticesAndTriangles
         && NofTriangles + 2 <= maxNofVerticesAndTriangles;
 
-    public void StartPipe(Direction direction, Vec2 position, float hue)
+    public void StartPipe(Direction direction, Vector2 position, float hue)
     {
         if (!CanStartPipe()) throw new InvalidOperationException("Not enough space to start a new pipe");
 
@@ -60,44 +58,28 @@ public class State(int maxNofVerticesAndTriangles, float pipeWidth, float speed)
             _ => (-speed, 0),
         };
         Hue = hue;
-        Color =
-        (
-            .5f + .5f * MathF.Cos(hue),
-            .5f + .5f * MathF.Cos(hue + MathF.PI * 2f / 3f),
-            .5f + .5f * MathF.Cos(hue + MathF.PI * 4f / 3f)
-        );
+        Color = Color.FromHue(hue);
 
-        uint begin0, begin1, begin2, begin3;
+        Vector2 pipeOffset = direction is Direction.North or Direction.South ? (pipeWidth, 0) : (0, pipeWidth);
 
-        switch (direction)
-        {
-            case Direction.North or Direction.South:
-                begin0 = AddVertex((position.X - pipeWidth, position.Y));
-                begin1 = AddVertex((position.X + pipeWidth, position.Y));
-                begin2 = AddVertex((position.X - pipeWidth, position.Y));
-                begin3 = AddVertex((position.X + pipeWidth, position.Y));
-                break;
-            default: // East or West
-                begin0 = AddVertex((position.X, position.Y - pipeWidth));
-                begin1 = AddVertex((position.X, position.Y + pipeWidth));
-                begin2 = AddVertex((position.X, position.Y - pipeWidth));
-                begin3 = AddVertex((position.X, position.Y + pipeWidth));
-                break;
-        }
+        var begin0 = AddVertex(position - pipeOffset);
+        var begin1 = AddVertex(position + pipeOffset);
+        var begin2 = AddVertex(position - pipeOffset);
+        var begin3 = AddVertex(position + pipeOffset);
 
         AddTriangle(begin0, begin1, begin2);
         AddTriangle(begin1, begin2, begin3);
     }
 
-    private uint AddVertex(Vec2 position)
+    private uint AddVertex(Vector2 position)
     {
-        Vertices[FloatsPerVertex * NofVertices + VertexProperty.X] = position.X * Scale.X;
-        Vertices[FloatsPerVertex * NofVertices + VertexProperty.Y] = position.Y * Scale.Y;
+        Vertices[FloatsPerVertex * NofVertices + VertexProperty.X] = position.X * Scale;
+        Vertices[FloatsPerVertex * NofVertices + VertexProperty.Y] = position.Y;
         Vertices[FloatsPerVertex * NofVertices + VertexProperty.Z] = 0.0f;
 
-        Vertices[FloatsPerVertex * NofVertices + VertexProperty.R] = Color.R;
-        Vertices[FloatsPerVertex * NofVertices + VertexProperty.G] = Color.G;
-        Vertices[FloatsPerVertex * NofVertices + VertexProperty.B] = Color.B;
+        Vertices[FloatsPerVertex * NofVertices + VertexProperty.R] = Color.Red;
+        Vertices[FloatsPerVertex * NofVertices + VertexProperty.G] = Color.Green;
+        Vertices[FloatsPerVertex * NofVertices + VertexProperty.B] = Color.Blue;
 
         return NofVertices++;
     }
@@ -126,13 +108,13 @@ public class State(int maxNofVerticesAndTriangles, float pipeWidth, float speed)
         StartPipe(direction, Position, Hue);
     }
 
-    private void CreateSphere(Vec2 position, float radius)
+    private void CreateSphere(Vector2 position, float radius)
     {
         var center = AddVertex(position);
         var circle = Enumerable.Range(0, SpherePrecision)
             .Select(i => i * 2.0f * MathF.PI / SpherePrecision)
-            .Select(angle => (X: radius * MathF.Cos(angle), Y: radius * MathF.Sin(angle)))
-            .Select(offset => AddVertex((position.X + offset.X, position.Y + offset.Y)))
+            .Select(angle => new Vector2(radius * MathF.Cos(angle), radius * MathF.Sin(angle)))
+            .Select(offset => AddVertex(position + offset))
             .ToList();
 
         for (int i = 0; i < SpherePrecision - 1; i++)
@@ -147,12 +129,12 @@ public class State(int maxNofVerticesAndTriangles, float pipeWidth, float speed)
     {
         if (!Started) throw new InvalidOperationException("Start a pipe first");
 
-        Position = (Position.X + Velocity.X, Position.Y + Velocity.Y);
+        Position += Velocity;
 
-        Vertices[FloatsPerVertex * (NofVertices - 2) + VertexProperty.X] += Velocity.X * Scale.X;
-        Vertices[FloatsPerVertex * (NofVertices - 2) + VertexProperty.Y] += Velocity.Y * Scale.Y;
+        Vertices[FloatsPerVertex * (NofVertices - 2) + VertexProperty.X] += Velocity.X * Scale;
+        Vertices[FloatsPerVertex * (NofVertices - 2) + VertexProperty.Y] += Velocity.Y;
 
-        Vertices[FloatsPerVertex * (NofVertices - 1) + VertexProperty.X] += Velocity.X * Scale.X;
-        Vertices[FloatsPerVertex * (NofVertices - 1) + VertexProperty.Y] += Velocity.Y * Scale.Y;
+        Vertices[FloatsPerVertex * (NofVertices - 1) + VertexProperty.X] += Velocity.X * Scale;
+        Vertices[FloatsPerVertex * (NofVertices - 1) + VertexProperty.Y] += Velocity.Y;
     }
 }
